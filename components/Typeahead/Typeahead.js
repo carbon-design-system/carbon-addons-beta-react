c5b1729223e4a2d79a23837063861ae5dc6032c9
Josh Black <joshblack@us.ibm.com>
2017-11-01 13:45:01 -0500

    fix(components): update Typeahead
    Fix the following issues brought up for Typeahead:
    
    - When clicking the menu icon, all items should be visible
    - Low contrast color for border on menu icon

--- components/Typeahead/Typeahead.js
+++ components/Typeahead/Typeahead.js
@@ -10,9 +10,20 @@ export default class Typeahead extends Component {
     onChange: () => {},
   };
 
+  state = {
+    isMenuOpen: false,
+  }
+
+
   handleChange = evt => {
     this.props.onChange(evt);
   };
+
+  handleOnClick = (...args) => {
+    this.setState(state => ({
+      isMenuOpen: !state.isMenuOpen,
+    }));
+  }
 
   itemToString = item => (item ? item.label : '');
 
@@ -29,8 +40,7 @@ export default class Typeahead extends Component {
 
   render() {
     const {
-      items,
-      placeholder,
+      items, placeholder,
       disabled,
       startsWith,
       id,
@@ -61,11 +71,6 @@ export default class Typeahead extends Component {
           const iconClearClass = classNames({
             'bx--typeahead-clear-icon': true,
           });
-          const itemsClassName = classNames({
-            'bx--typeahead-items': true,
-            'bx--typeahead-items--expanded': isOpen,
-          });
-
           return (
             <div>
               <div className="bx--typeahead bx--form-item">
@@ -77,70 +82,125 @@ export default class Typeahead extends Component {
                     placeholder,
                   })}
                 />
-                {inputValue && (
-                  <button
-                    onClick={clearSelection}
-                    className="bx--typeahead-clear"
-                  >
-                    <svg
-                      className={iconClearClass}
-                      width="16"
-                      height="16"
-                      viewBox="0 0 16 16"
-                      fillRule="evenodd"
-                    >
-                      <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm3.5 10.1l-1.4 1.4L8 9.4l-2.1 2.1-1.4-1.4L6.6 8 4.5 5.9l1.4-1.4L8 6.6l2.1-2.1 1.4 1.4L9.4 8l2.1 2.1z" />
-                    </svg>
-                  </button>
-                )}
-                <button
-                  className="bx--typeahead-menu"
-                  {...getButtonProps({
-                    'aria-label': isOpen ? 'close.menu' : 'open.menu',
-                  })}
-                >
-                  <svg
-                    className={iconMenuClass}
-                    width="10"
-                    height="5"
-                    viewBox="0 0 10 5"
-                    fillRule="evenodd"
-                  >
-                    <path d="M10 0L5 5 0 0z" />
-                  </svg>
-                </button>
-                <div className={itemsClassName}>
-                  {isOpen
-                    ? items
-                        .filter(item =>
-                          this.filterItems(inputValue, item, startsWith)
-                        )
-                        .map((item, index) => {
-                          const itemClass = classNames({
-                            'bx--typeahead__item': true,
-                            'bx--typeahead__item--highlighted':
-                              highlightedIndex === index,
-                            'bx--typeahead__item--active':
-                              selectedItem === item,
-                          });
-
-                          return (
-                            <div
-                              {...getItemProps({ item })}
-                              key={item.value}
-                              className={itemClass}
-                            >
-                              {itemToString(item)}
-                            </div>
-                          );
-                        })
-                    : null}
-                </div>
+                {this.renderClear(inputValue, clearSelection)}
+                {this.renderMenuToggle(isOpen, getButtonProps)}
+                {this.renderItems({
+                  items,
+                  isOpen,
+                  inputValue,
+                  getItemProps,
+                  itemToString,
+                  highlightedIndex,
+                  selectedItem,
+                  startsWith,
+                })}
               </div>
             </div>
           );
         }}
       </Downshift>
+    );
+  }
+
+  renderMenuToggle = (isOpen, getButtonProps) => {
+    const iconMenuClass = classNames({
+      'bx--typeahead-menu-icon': true,
+      'bx--typeahead-menu-icon--active': isOpen,
+    });
+
+    return (
+      <button
+        className="bx--typeahead-menu"
+        {...getButtonProps({
+          'aria-label': isOpen ? 'close.menu' : 'open.menu',
+          onClick: this.handleOnClick,
+        })}
+      >
+        <svg
+          className={iconMenuClass}
+          width="10"
+          height="5"
+          viewBox="0 0 10 5"
+          fillRule="evenodd"
+        >
+          <path d="M10 0L5 5 0 0z" />
+        </svg>
+      </button>
+    );
+  }
+
+  renderClear = (inputValue, clearSelection) => {
+    if (!inputValue) {
+      return null;
+    }
+    return (
+      <button
+        onClick={clearSelection}
+        className="bx--typeahead-clear">
+        <svg
+          className="bx--typeahead-clear-icon"
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fillRule="evenodd"
+        >
+          <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm3.5 10.1l-1.4 1.4L8 9.4l-2.1 2.1-1.4-1.4L6.6 8 4.5 5.9l1.4-1.4L8 6.6l2.1-2.1 1.4 1.4L9.4 8l2.1 2.1z" />
+        </svg>
+      </button>
+    );
+  }
+
+  renderItems = ({
+    items,
+    isOpen,
+    inputValue,
+    getItemProps,
+    itemToString,
+    highlightedIndex,
+    selectedItem,
+    startsWith,
+  }) => {
+    const itemsClassName = classNames({
+      'bx--typeahead-items': true,
+      'bx--typeahead-items--expanded': isOpen,
+    });
+    const children = items
+      .filter(item => {
+        if (!isOpen) {
+          return false;
+        }
+
+        if (this.state.isMenuOpen) {
+          return true;
+        }
+
+        return this.filterItems(inputValue, item, startsWith);
+      })
+      .map((item, index) => {
+        const itemClass = classNames({
+          'bx--typeahead__item': true,
+          'bx--typeahead__item--highlighted': highlightedIndex === index,
+          'bx--typeahead__item--active': selectedItem === item,
+        });
+
+        return (
+          <div
+            {...getItemProps({
+              item,
+              onClick: this.handleOnClick,
+            })}
+            key={item.value}
+            className={itemClass}
+          >
+            {itemToString(item)}
+          </div>
+        );
+      });
+
+    return (
+      <div className={itemsClassName}>
+        {children}
+      </div>
     );
   }
 }
